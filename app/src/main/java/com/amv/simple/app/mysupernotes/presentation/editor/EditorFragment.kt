3c99bd3936f83ee75486be7e0409e9bdc13b222b
Максим Андрейченко <94169040+amv0107@users.noreply.github.com>
2024-03-20 22:53:07 +0200

    create design formation menu

--- app/src/main/java/com/amv/simple/app/mysupernotes/presentation/editor/EditorFragment.kt
+++ app/src/main/java/com/amv/simple/app/mysupernotes/presentation/editor/EditorFragment.kt
@@ -9,6 +9,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.view.menu.MenuBuilder
