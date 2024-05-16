ab228371a0f3580c1b6ba26bd50b38695111c1dd
Максим Андрейченко <94169040+amv0107@users.noreply.github.com>
2024-05-17 01:37:19 +0300

    create add to db

--- app/src/main/java/com/amv/simple/app/mysupernotes/data/AppDatabase.kt
+++ app/src/main/java/com/amv/simple/app/mysupernotes/data/AppDatabase.kt
@@ -2,17 +2,23 @@ package com.amv.simple.app.mysupernotes.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.amv.simple.app.mysupernotes.data.category.CategoryDbModel
+import com.amv.simple.app.mysupernotes.data.category.CategoryItemDao
 import com.amv.simple.app.mysupernotes.data.note.NoteItemDao
 import com.amv.simple.app.mysupernotes.data.note.NoteItemDbModel
 
 @Database(
-    entities = [NoteItemDbModel::class],
+    entities = [
+        NoteItemDbModel::class,
+        CategoryDbModel::class,
+    ],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteItemDao
+    abstract fun categoryDao(): CategoryItemDao
 
     companion object {
 
